77ec9094d928e7a4db5e4a10a3de94fda24e20d7
Pyae Sone Aung <pyaesoneaung.code@gmail.com>
2022-04-13 17:54:42 +0630

    add github link

--- resources/views/filament/widgets/demo-information.blade.php
+++ resources/views/filament/widgets/demo-information.blade.php
@@ -3,5 +3,6 @@
         <p>A demo application made with Filament (TALLkit for Laravel).<p>
         <p>Database is reset every 30 minutes.<p>
         <p>Please be <span style="color: red">respectful</span> when editing data.</p>
+        <p>Source Code is available at <a style="color: blue" href="https://github.com/PyaeSoneAungRgn/organic-cms">PyaeSoneAungRgn/organic-cms</a>.</p>
     </x-filament::card>
 </x-filament::widget>
